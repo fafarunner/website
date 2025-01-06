07d4f602f5dfef26639955c53673796003db3c54
kjxbyz <kjxbyz888@outlook.com>
2025-01-06 12:00:24 +0800

    fix: remove visitor badge

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/image";
 import Link from "next/link";
 import { useTranslation } from "@/i18n/client";
 import type { LngProps } from "@/types/i18next-lng";
@@ -50,12 +49,6 @@ export default function Footer(props: LngProps) {
         {VERCEL_GIT_COMMIT_SHA && (
           <>{VERCEL_GIT_COMMIT_SHA.substring(0, 8)}&nbsp;</>
         )}
-        <Image
-          src="https://visitor-badge.laobi.icu/badge?page_id=fafa-runner.chenyifaer.com"
-          width={60}
-          height={20}
-          alt="visitor badge"
-        />
       </span>
     </div>
   );
